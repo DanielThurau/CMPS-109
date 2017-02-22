ac1a4a343d8411215103a1202d4f9ec324dcd162
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2017-02-22 12:11:30 -0800

    KB Inference query working

--- inference/Inference.cpp
+++ inference/Inference.cpp
@@ -3,10 +3,40 @@
 Inference::Inference(KnowledgeBase * p_KB, RuleBase * p_RB){
 	KB = p_KB;
 	RB = p_RB;
+	std::cout << "Created Interface Object\n";
 }
 
 std::set<std::vector<std::string>> Inference::query(std::vector<std::string> p_Inference){
+	int inferenceSize = p_Inference.size();
+
+	std::cout << "In the Interface query\n";
+	
 	std::set<std::vector<std::string>> results;
+	std::vector<Fact*> data = KB->getContent(p_Inference[0]);
+	for(int i = 0; i < data.size(); ++i){
+		if(data[i]->getFact().size() == inferenceSize){
+			std::vector<std::string > temp_fact = data[i]->getFact();
+			std::vector<std::string > temp;
+			for(int j = 0; j < inferenceSize; ++j){
+				temp.push_back(temp_fact[j]);
+			}
+			results.insert(temp);
+		}
+	}
+
+	for(auto it = results.begin(); it != results.end(); ++it){
+		std::vector<std::string > v = *it;
+		for(int p = 1 ; p < v.size(); p++){
+			if(p == v.size()-1){
+				std::cout << p_Inference[p] << ":" << v[p] << " ";
+			}else{
+				std::cout << p_Inference[p] << ":" << v[p] << ", ";
+			}
+		}
+		std::cout << "\n";
+	}
+
+
 	return results;
 }
 
@@ -14,15 +44,3 @@ Inference::~Inference(){
 	free(KB);
 	free(RB);
 }
-
-
-
-// class Inference{
-// 	private:
-// 		RuleBase * RB;
-// 		KnowledgeBase * KB;
-// 	public:
-// 		Inference(KnowledgeBase * KB, RuleBase * RB);
-// 		std::set<std::vector<std::string>> query(std::vector<std::string>);
-// 		~Inference();
-// }
--- inference/main.cpp
+++ inference/main.cpp
@@ -32,7 +32,45 @@ int main(){
 	std::cout << "Adding to Rulebase: " << RB->addContent(r3) << '\n';
 	std::cout << "Adding to Rulebase: " << RB->addContent(r4) << '\n';
 	std::cout << "Adding to Rulebase: " << RB->addContent(r5) << '\n';
+	
+
+
 	//******************************************************************************
+	// KnowledgeBase Creation
+	//******************************************************************************
+
+	KnowledgeBase * KB = new KnowledgeBase();
+
+	std::vector<std::string > v6 = {"Father", "Thoma", "bob"};
+	std::vector<std::string > v7 = {"Mother", "Talitha", "sally"};
+	std::vector<std::string > v8 = {"Grandfather", "Robert", "Greg"};
+	std::vector<std::string > v9 = {"Grandfather", "Robert", "Martha"};
+	std::vector<std::string > v10 = {"Father", "Thoma", "Emma"};
+
+	Fact * f1 = new Fact(v6);
+	Fact * f2 = new Fact(v7);
+	Fact * f3 = new Fact(v8);
+	Fact * f4 = new Fact(v9);
+	Fact * f5 = new Fact(v10);
+
+	std::cout << "Adding to KnowledgeBase: " << KB->addContent(f1) << '\n';
+	std::cout << "Adding to KnowledgeBase: " << KB->addContent(f2) << '\n';
+	std::cout << "Adding to KnowledgeBase: " << KB->addContent(f3) << '\n';
+	std::cout << "Adding to KnowledgeBase: " << KB->addContent(f4) << '\n';
+	std::cout << "Adding to KnowledgeBase: " << KB->addContent(f5) << '\n';
+
+
+
+	//*******************************************************************************
+	// Creating Inference object
+	//*******************************************************************************
+
+	Inference * test = new Inference(KB, RB);
+	
+	std::vector<std::string > i1 = {"Grandfather", "$X","$Y"};
+
+	std::set<std::vector<std::string>> a1 = test->query(i1);
+	delete(test);
 
 	return 0;
 }
