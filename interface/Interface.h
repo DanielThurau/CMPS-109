f6929355db6f721454e72d9a6e35841662c89754
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2017-02-28 02:56:30 -0800

    idek

--- interface/Interface.h
+++ interface/Interface.h
@@ -14,20 +14,14 @@ class Interface {
 		KnowledgeBase * KB;
 		RuleBase * RB;
 	public: 
-		/* Basic constructor. Calls constructor on KB and RB */
 		Interface();
-		/* May be removed, paramatarized constructor.  Assigns ptrs */
 		Interface(KnowledgeBase * p_KB, RuleBase * p_RB);
-		/* commandLine().  Session for user, loops after each commnd */
 		void commandLine();
-		/* executecommand, given a 2d vector of strings, execute a set of
-		commands depending on what is passed */
-		bool executeCommand(std::vector<std::vector<std::string>> p_command);
-		/* PArse SEG? */
+		bool executeCommand(std::vector<std::vector<std::string>>
+								p_command);
 		std::vector<std::string> parseSeg(std::string target);
-		/* parse takes in a string and returns a 2d vector of strings */
-		bool parse(std::string p_statment);
-		/* destructor for interface */
+		std::vector<std::vector<std::string>> parse(std::string 
+								p_statment);
 		virtual ~Interface();
 };
 #endif
