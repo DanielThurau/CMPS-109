500f6bd653f0ee8e72f02beef7de086b0dbff06d
Andres Segundo <aisegund@ucsc.edu>
2017-03-15 22:09:22 -0700

    interface header

--- client/headers/Interface.h
+++ client/headers/Interface.h
@@ -17,7 +17,19 @@ class Interface {
 		// parser helper method
 		TCPSocket * mySocket;
 		
-	public: 
+	public:
+		//temp buffer used to read from socket
+		//char * temp_buffer;
+		//gets the integers found at the beginning of char*
+		int argSize(char * p_arg, int split);
+		//converts char * to string
+		string convertToStr(char * c_arg);
+		//returns 2d vector in format accepted by print_query
+		std::vector<std::vector<std::string>> parseSeg(string target, int targetNum, int argNum);
+		//method copied from inference 
+		void print_query(std::vector<std::vector<std::string>> p_set);
+		//formats string so that it can be passed to printquery
+		void unformat(char * p_arg);
 		// interface constructor 
 		Interface();
 		// looping command line for user
